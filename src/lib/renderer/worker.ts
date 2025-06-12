684bc2a54eb8b11c3e37ae2eecd084fda00867f1
Ed Sunman <ed.sunman@freshlabel.co.uk>
2025-06-12 16:02:52 +0100

    play and pause working

--- src/lib/renderer/worker.ts
+++ src/lib/renderer/worker.ts
@@ -9,6 +9,8 @@ import {
 } from 'mp4box';
 import { WebGPURenderer } from './renderer';
 
+const DEBUG_QUEUES = false;
+
 const chunks: EncodedVideoChunk[] = [];
 let renderer: WebGPURenderer | null = null;
 let decoder: VideoDecoder | null = null;
@@ -18,8 +20,6 @@ let targetFrame = 0;
 let selectedFrameTimestamp = 0;
 let playing = false;
 let feedMoreFrames = false;
-let frameTime = 0;
-
 let frameQueue: VideoFrame[] = []; // Holds decoded VideoFrames waiting to be rendered
 let encodedChunkBuffer: EncodedVideoChunk[] = []; // Holds EncodedVideoChunks ready to be fed to the decoder
 let isFeedingPaused = false;
@@ -36,13 +36,6 @@ self.addEventListener('message', async function (e) {
 				renderer = new WebGPURenderer(e.data.canvas);
 				decoder = new VideoDecoder({
 					output(frame) {
-						//console.log(frame.timestamp);
-						//frameQueue.push(frame);
-						// As soon as a frame is outputted, try to render it and potentially resume feeding
-						//renderNextFrame(); // Your function to draw the frame
-
-						// After a frame is processed (and its memory released via videoFrame.close()),
-						// we can check if we should resume feeding.
 						if (playing) {
 							//frameQueue.push(frame);
 							//console.log(frame);
@@ -51,7 +44,7 @@ self.addEventListener('message', async function (e) {
 							} else if (frame.timestamp === selectedFrameTimestamp) {
 								renderer?.draw(frame);
 								feedMoreFrames = true;
-								console.log('drawn start');
+								//	console.log('drawn start');
 							} else {
 								frame.close();
 							}
@@ -68,10 +61,10 @@ self.addEventListener('message', async function (e) {
 
 						if (isFeedingPaused && /* frameQueue.length < 10 &&  */ decoder!.decodeQueueSize < 3) {
 							isFeedingPaused = false;
-							console.log('Decoder backpressure: Resuming feeding.');
+							if (DEBUG_QUEUES) console.log('Decoder backpressure: Resuming feeding.', playing);
 							feedDecoder();
 						}
-						console.log('Queue size: ', decoder!.decodeQueueSize);
+						if (DEBUG_QUEUES) console.log('Decoder queue size: ', decoder!.decodeQueueSize);
 					},
 					error(e) {
 						console.log('encoder error', e);
@@ -140,11 +133,8 @@ self.addEventListener('message', async function (e) {
 				playing = true;
 				frameQueue = [];
 				encodedChunkBuffer = [];
-				//decoder?.flush();
+
 				let firstRAFTimestamp: number | null = null;
-
-				//console.log(selectedFrameTimestamp);
-
 				let targetFrameIndex = 0;
 				let keyFrameIndex = 0;
 				let scanForKeyframe = false;
@@ -162,15 +152,10 @@ self.addEventListener('message', async function (e) {
 					}
 				}
 
-				//frameQueue = [];
-				//	console.log(frameQueue);
-				const c = [];
 				for (let i = keyFrameIndex; i < targetFrameIndex + 10; i++) {
 					encodedChunkBuffer.push(chunks[i]);
 					lastChunkIndex = i;
-					c.push(chunks[i]);
 				}
-				console.log(c);
 
 				feedDecoder();
 
@@ -185,8 +170,8 @@ self.addEventListener('message', async function (e) {
 						const elapsedTimeMs = rafTimestamp - firstRAFTimestamp;
 						const frameTime = Math.floor(elapsedTimeMs * 1000) + selectedFrameTimestamp;
 
-						console.log('-- new loop, frame queue length ', frameQueue.length);
-						console.log('chunk buffer ', encodedChunkBuffer.length);
+						if (DEBUG_QUEUES) console.log('-- new loop, frame queue length ', frameQueue.length);
+						//	console.log('chunk buffer ', encodedChunkBuffer.length);
 
 						let minTimeDelta = Infinity;
 						let frameIndex = -1;
@@ -202,27 +187,30 @@ self.addEventListener('message', async function (e) {
 						}
 
 						for (let i = 0; i < frameIndex; i++) {
-							const staleFrame = frameQueue.shift();
+							/* const staleFrame =  */ frameQueue.shift();
 							//console.log('stale frame was closed: ', staleFrame?.format);
-							//	if (staleFrame) console.log(staleFrame);
 							//staleFrame?.close();
 						}
 
 						const chosenFrame = frameQueue[0];
+						//lastFramePlayed = chosenFrame;
 
 						if (chosenFrame && chosenFrame.format) {
-							console.log(
-								'Sending to render. Frame time delta = %dms (%d vs %d)',
-								minTimeDelta / 1000,
-								frameTime,
-								chosenFrame.timestamp
-							);
+							if (DEBUG_QUEUES)
+								console.log(
+									'Sending to render. Frame time delta = %dms (%d vs %d)',
+									minTimeDelta / 1000,
+									frameTime,
+									chosenFrame.timestamp
+								);
 							//console.log(chosenFrame);
+							targetFrame = frameQueue[0].timestamp;
 							renderer?.draw(chosenFrame);
 						}
 
 						if (encodedChunkBuffer.length < 5) {
-							console.log('fill buffer starting with chunk index ', lastChunkIndex + 1);
+							if (DEBUG_QUEUES)
+								console.log('fill chunk buffer starting with index ', lastChunkIndex + 1);
 
 							for (let i = lastChunkIndex + 1, j = 0; j < 10; i++, j++) {
 								//	console.log('pushing chunk ', i);
@@ -235,24 +223,6 @@ self.addEventListener('message', async function (e) {
 						}
 					}
 
-					//let targetFrameIndex = 0;
-					/* 						let minTimeDelta = Infinity;
-						for (let i = 0; i < chunks.length; i++) {
-							const time_delta = Math.abs(frameTime - chunks[i].timestamp);
-							if (time_delta < minTimeDelta) {
-								minTimeDelta = time_delta;
-								//targetFrameIndex = i;
-								selectedFrameTimestamp = chunks[i].timestamp;
-							} else {
-								break;
-							}
-						} */
-
-					//console.log('elapsed', elapsedTimeMs);
-					//console.log('looking for', frameTime);
-					//console.log('found', selectedFrameTimestamp);
-					//console.log(targetFrameIndex);
-
 					self.requestAnimationFrame(loop);
 				};
 				self.requestAnimationFrame(loop);
@@ -263,18 +233,19 @@ self.addEventListener('message', async function (e) {
 		case 'pause':
 			playing = false;
 			feedMoreFrames = false;
-			console.log('paused', frameQueue);
+			isFeedingPaused = false;
+			//console.log('Paused', JSON.stringify(frameQueue));
+			if (DEBUG_QUEUES) console.log('Paused. Frames left in queue:', frameQueue.length);
 
 			for (let i = 0; i < frameQueue.length; i++) {
-				const staleFrame = frameQueue.shift();
-
-				staleFrame?.close();
+				//const staleFrame = frameQueue.shift();
+				frameQueue[i].close();
+				if (DEBUG_QUEUES) console.log('Closed frame: ', frameQueue[i]);
+				//console.log('closed', staleFrame);
+				//	staleFrame?.close();
 			}
-			//console.log('flush started');
-			//decoder?.flush();
-			//	console.log('flush finished');
-			//isFeedingPaused = false;
-			//decoder?.flush();
+			frameQueue = [];
+
 			break;
 		case 'seek':
 			{
@@ -285,13 +256,8 @@ self.addEventListener('message', async function (e) {
 				}
 
 				seeking = true;
-				console.log('attempting to seek. Queue: ', decoder.decodeQueueSize);
-				//decoder.flush();
-				/* console.log('flush started');
-				
-				console.log('flush finished'); */
+
 				targetFrame = Math.floor(e.data.targetFrame * 33333.3333333) + 33333 / 2;
-				//console.log(`target frame is ${targetFrame}`);
 
 				let targetFrameIndex = 0;
 				let keyFrameIndex = 0;
@@ -311,16 +277,11 @@ self.addEventListener('message', async function (e) {
 				}
 
 				encodedChunkBuffer = [];
-
-				//frameQueue = [];
-				//	console.log(frameQueue);
-				let c = [];
 				for (let i = keyFrameIndex; i < targetFrameIndex + 10; i++) {
 					encodedChunkBuffer.push(chunks[i]);
 					lastChunkIndex = i;
-					c.push(chunks[i]);
 				}
-				console.log(c);
+
 				isFeedingPaused = false;
 				feedDecoder();
 			}
@@ -332,40 +293,35 @@ const feedDecoder = () => {
 	if (isFeedingPaused || !decoder || decoder.state !== 'configured') {
 		return; // Don't feed if paused or not configured
 	}
-	console.log('Queue size: ', decoder.decodeQueueSize);
+	//console.log('Queue size: ', decoder.decodeQueueSize);
 	// Check if we're hitting our backpressure limits
 	// 1. Too many decoded frames waiting to be rendered
 	// 2. Too many chunks already sent to the decoder, but not yet outputted
 	if (/* frameQueue.length >= 10 ||  */ decoder.decodeQueueSize >= 3) {
 		isFeedingPaused = true;
-		console.log(
-			'Decoder backpressure: Pausing feeding. frameQueue:',
-			frameQueue.length,
-			'decodeQueueSize:',
-			decoder.decodeQueueSize
-		);
+		if (DEBUG_QUEUES)
+			console.log(
+				'Decoder backpressure: Pausing feeding. frameQueue:',
+				frameQueue.length,
+				'decodeQueueSize:',
+				decoder.decodeQueueSize
+			);
 		return; // Stop feeding for now
 	}
 
 	// If we have chunks in our buffer and not paused, send them
-	if (encodedChunkBuffer.length > 0) {
-		const chunk = encodedChunkBuffer.shift(); // Get the next chunk
+	if ((playing || seeking) && encodedChunkBuffer.length > 0) {
+		const chunk = encodedChunkBuffer.shift();
 		if (!chunk) return;
 		try {
 			decoder.decode(chunk);
-			console.log('sending chunk to encoder: ', chunk.timestamp);
-			// Successfully sent, try to send more if conditions allow
 			feedDecoder();
 		} catch (e) {
 			console.error('Error decoding chunk:', e);
-			// Handle fatal errors or corrupted chunks
-			//  videoDecoder.close(); // Consider closing on fatal error
 		}
 	} else {
-		console.log('no more chunks');
+		if (DEBUG_QUEUES) console.log('No more chunks in the buffer to feed');
 		seeking = false;
-		// No more chunks in the buffer to feed right now.
-		// This might be the end of the video segment, or we're waiting for more data from network/file.
 	}
 };
 
