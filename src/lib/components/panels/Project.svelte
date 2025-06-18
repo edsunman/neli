2e7e34a56b3b557d81e38a9ee70f76a38658f257
Ed Sunman <ed.sunman@freshlabel.co.uk>
2025-06-18 14:27:02 +0100

    more ui improvements

--- src/lib/components/panels/Project.svelte
+++ src/lib/components/panels/Project.svelte
@@ -15,35 +15,37 @@
 </script>
 
 <table
-	class="text-zinc-500 text-sm pl-[calc(100svw/20)] p-12 w-full block border-spacing-y-3 select-none"
+	class="text-zinc-500 text-sm pl-[calc(100svw/20)] p-12 w-full block border-separate border-spacing-y-1 select-none"
 >
 	<tbody>
 		{#each { length: 3 } as _}
-			<tr class="group">
-				<td class="py-2 group-hover:bg-zinc-800">
-					<div
-						class="h-8 w-12 bg-amber-300 rounded-sm opacity-70 group-hover:opacity-100 transition-opacity"
-					></div>
+			<tr class="group hover:bg-zinc-800">
+				<td class="py-2 group-hover:bg-zinc-800 rounded-l pl-2">
+					<div class="h-8 w-12 bg-amber-300 rounded-sm opacity-70 group-hover:opacity-100"></div>
 				</td>
-				<td class="pl-3 group-hover:text-zinc-300 group-hover:bg-zinc-800">nameOfVideo.mp4</td>
-				<td class="pl-4 group-hover:bg-zinc-800"
+				<td class="pl-3 group-hover:text-zinc-300">nameOfVideo.mp4</td>
+				<td class="pl-4"
 					><small class="bg-zinc-600 group-hover:bg-zinc-300 text-zinc-900 py-0.5 px-1 rounded-sm"
 						>1:00</small
 					></td
 				>
-				<td class="pl-4 group-hover:text-zinc-300 group-hover:bg-zinc-800"><small>16:9</small></td>
-				<td class="pl-4 group-hover:text-zinc-300 group-hover:bg-zinc-800"><small>30fps</small></td>
+				<td class="pl-4 group-hover:text-zinc-300"><small>16:9</small></td>
+				<td class="pl-4 group-hover:text-zinc-300 rounded-r pr-2"><small>30fps</small></td>
 			</tr>
 		{/each}
 		{#each appState.sources as source}
-			<tr class="group" onclick={() => createClip(source.id, timelineState.currentFrame, 500)}>
-				<td class="py-2"
+			<tr
+				class="group hover:bg-zinc-800"
+				onclick={() => createClip(source.id, timelineState.currentFrame, 500)}
+			>
+				<td class="py-2 rounded-l pl-2"
 					><div
 						class="h-8 w-12 bg-green-300 rounded-sm opacity-70 group-hover:opacity-100 transition-opacity"
 					></div></td
 				>
-				<td class="pl-3 group-hover:text-zinc-300" colspan={source.type === 'text' ? 4 : 1}
-					>{source.name}</td
+				<td
+					class="pl-3 group-hover:text-zinc-300 rounded-r pr-2"
+					colspan={source.type === 'text' ? 4 : 1}>{source.name}</td
 				>
 				{#if source.type === 'video'}
 					<td class="pl-3">16:9</td>
--- src/lib/components/panels/Settings.svelte
+++ src/lib/components/panels/Settings.svelte
@@ -1,11 +1,42 @@
 <script lang="ts">
 	import { timelineState } from '$lib/state.svelte';
-	import { Slider } from 'bits-ui';
+	import { Slider, ToggleGroup } from 'bits-ui';
 </script>
 
 {#if !timelineState.selectedClip}
-	<div class="flex flex-col mt-12 mr-[calc(100svw/20)] rounded text-zinc-500 text-right">
+	<div class="flex flex-col mt-12 mr-[calc(100svw/20)] ml-[30%] rounded text-zinc-500 text-right">
 		<div class="text-lg">project name</div>
+		<div class="flex flex-col gap-3 mt-4">
+			<div class="flex items-center justify-between text-sm font-medium">
+				<span>Aspect ratio</span>
+				<span>
+					<ToggleGroup.Root type="multiple" class="flex items-center gap-x-1 px-[4px]">
+						{#each { length: 3 } as _}
+							<ToggleGroup.Item
+								aria-label="toggle bold"
+								value="bold"
+								class="rounded-sm hover:bg-zinc-800 hover:text-white active:bg-zinc-700 data-[state=on]:bg-zinc-700 data-[state=off]:text-foreground-alt data-[state=on]:text-foreground active:data-[state=on]:bg-dark-10 inline-flex size-8 items-center justify-center transition-all"
+							>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" class="size-6"
+									><rect width="256" height="256" fill="none" /><rect
+										x="78"
+										y="32"
+										width="100"
+										height="192"
+										rx="8"
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="18"
+									/></svg
+								>
+							</ToggleGroup.Item>
+						{/each}
+					</ToggleGroup.Root>
+				</span>
+			</div>
+		</div>
 	</div>
 {/if}
 
@@ -13,7 +44,7 @@
 	{#if clip.id === timelineState.selectedClip?.id}
 		<div class="flex flex-col mt-12 mr-[calc(100svw/20)] ml-[30%] rounded text-zinc-500 text-right">
 			<div class="text-lg">{clip.source.type}</div>
-			<div class="flex flex-col gap-4 mt-4">
+			<div class="flex flex-col gap-3 mt-4">
 				<div class="flex items-center justify-between text-sm font-medium">
 					<span>Volume</span>
 					<span>50%</span>
@@ -24,13 +55,13 @@
 					class="relative flex w-full touch-none select-none items-center"
 				>
 					<span
-						class="bg-dark-10 relative h-1 w-full grow cursor-pointer overflow-hidden rounded-full"
+						class="bg-zinc-700 relative h-1 w-full grow cursor-pointer overflow-hidden rounded-full"
 					>
-						<Slider.Range class="bg-white absolute h-full" />
+						<Slider.Range class="bg-rose-600 absolute h-full" />
 					</span>
 					<Slider.Thumb
 						index={0}
-						class={'border-border-input bg-background hover:border-dark-40 focus-visible:ring-foreground dark:bg-foreground dark:shadow-card data-active:border-dark-40 focus-visible:outline-hidden data-active:scale-[0.98] block size-[15px] cursor-pointer rounded-full border shadow-sm transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'}
+						class={'bg-rose-600 ring-white focus-visible:ring-2  ring-offset-transparent focus-visible:ring-foreground focus-visible:outline-hidden block size-[15px] cursor-pointer rounded-full  '}
 					/>
 				</Slider.Root>
 			</div>
