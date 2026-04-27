5225a77faba49e5fb2adb59a53ddf8392c630b43
Ed Sunman <ed.sunman@freshlabel.co.uk>
2026-04-27 23:16:35 +0100

    update action and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,21 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, type PluginOption } from 'vite';
 import fs from 'fs';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 export default defineConfig({
-	server: {
-		host: '127.0.0.1',
-		allowedHosts: ['app.local.test', 'local.test'],
-		port: 5173,
-		strictPort: true,
-		https: {
-			key: fs.readFileSync('./key.pem'),
-			cert: fs.readFileSync('./cert.pem')
-		}
-	},
+	server: isDev
+		? {
+				host: '127.0.0.1',
+				allowedHosts: ['app.local.test', 'local.test'],
+				port: 5173,
+				strictPort: true,
+				https: {
+					key: fs.readFileSync('./key.pem'),
+					cert: fs.readFileSync('./cert.pem')
+				}
+			}
+		: undefined,
 	plugins: [tailwindcss() as PluginOption[], sveltekit()],
 	define: { __VERSION__: JSON.stringify(process.env.npm_package_version) }
 });
