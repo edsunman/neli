da5747d4d567191ab0e1b41ba7f95cfeee57d276
Ed Sunman <ed.sunman@freshlabel.co.uk>
2026-05-07 16:47:30 +0100

    size change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { appState, historyManager } from '$lib/state.svelte';
-	import { focusTrack } from '$lib/timeline/actions';
 
 	import Sources from '$lib/components/panels/Sources.svelte';
 	import Program from '$lib/components/panels/Program.svelte';
@@ -8,7 +7,6 @@
 	import Properties from '$lib/components/panels/Properties.svelte';
 	import Palette from '$lib/components/palette/Palette.svelte';
 	import DragAndDropIcon from '$lib/components/misc/DragAndDropIcon.svelte';
-	import { deselectAllClips } from '$lib/clip/actions.svelte';
 </script>
 
 <svelte:head>
@@ -24,7 +22,7 @@
 <div
 	inert={appState.palette.open}
 	id="portalContainer"
-	class="relative overflow-hidden h-dvh grid grid-cols-[20%_60%_20%] xl:grid-cols-[20%_60%_20%] grid-rows-[55%_45%] height-xl:grid-rows-[calc(100svh-350px)_350px] bg-zinc-900"
+	class="relative overflow-hidden h-dvh grid grid-cols-[20%_60%_20%] xl:grid-cols-[20%_60%_20%] grid-rows-[55%_45%] height-xl:grid-rows-[calc(100svh-360px)_360px] bg-zinc-900"
 >
 	<div>
 		<Sources />
